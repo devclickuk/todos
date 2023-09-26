cde8049b8a32231d6c7931ababe43e8afeb73d53
Dave Amison <dave.amison@sixstar.global>
2023-09-26 13:47:54 +0000

    basic app layout with state and ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { useState, useRef, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const textRef = useRef()
+
+  useEffect(() => {
+    setTodos([
+      {
+        id: uuidv4(),
+        text: 'Buy milk',
+        complete: false
+      },
+      {
+        id: uuidv4(),
+        text: 'Buy eggs',
+        complete: false
+      },
+      {
+        id: uuidv4(),
+        text: 'Buy bread',
+        complete: false
+      },
+    ])
+  }, [])
+
+
   return (
     <div className="App">
-      Page
+      <div className="listContainer">
+        {todos.map((todo) => (
+          <div key={todo.id} className="listItem">
+            <span className="itemText">{todo.text}</span>
+            <div className="itemDeleteIcon">X</div>
+          </div>
+        ))}
+      </div>
+      <div className="addContainer">
+        <input
+          className="addInput"
+          ref={textRef}
+          type="text"
+        />
+        <button className="addButton">Add</button>
+      </div>
     </div>
   );
 }
