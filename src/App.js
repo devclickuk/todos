b897232c1d2309a67de19ae2b32aa84a7a61fbb1
Dave Amison <dave.amison@sixstar.global>
2023-09-26 13:56:26 +0000

    adding and removing items

--- src/App.js
+++ src/App.js
@@ -26,6 +26,20 @@ function App() {
     ])
   }, [])
 
+  const handleAdd = () => {
+    console.log('handle add clicked')
+    const id = uuidv4()
+    setTodos([...todos, { id: id, text: textRef.current.value }])
+    resetTextValue()
+  }
+
+  const resetTextValue = () => {
+    textRef.current.value = ''
+  }
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
 
   return (
     <div className="App">
@@ -33,17 +47,22 @@ function App() {
         {todos.map((todo) => (
           <div key={todo.id} className="listItem">
             <span className="itemText">{todo.text}</span>
-            <div className="itemDeleteIcon">X</div>
+            <div className="itemDeleteIcon" onClick={() => handleDelete(todo.id)}>X</div>
           </div>
         ))}
       </div>
       <div className="addContainer">
         <input
           className="addInput"
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleAdd()
+            }
+          }}
           ref={textRef}
           type="text"
         />
-        <button className="addButton">Add</button>
+        <button className="addButton" onClick={handleAdd}>Add</button>
       </div>
     </div>
   );
