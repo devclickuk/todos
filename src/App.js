f0aa983dc50e356bc0b238f1f7c953b80ee28b3b
Dave Amison <dave.amison@sixstar.global>
2023-09-27 10:46:54 +0000

    add icons and toggle to complete functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useRef } from 'react'
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { addTask, deleteTask } from './redux/slices/tasksSlice';
+import { addTask, deleteTask, toggleTask } from './redux/slices/tasksSlice';
+import { RxCross2 } from 'react-icons/rx'
+import { BsCheckLg } from 'react-icons/bs'
+import { CiUndo } from 'react-icons/ci'
 
 function App() {
   const dispatch = useDispatch()
@@ -25,13 +28,25 @@ function App() {
     dispatch(deleteTask({ id }))
   }
 
+  const handleToggle = (id) => {
+    console.log(id)
+    dispatch(toggleTask({ id }))
+  }
+
   return (
     <div className="App">
       <div className="listContainer">
         {tasks.map((task) => (
           <div key={task.id} className="listItem">
-            <span className="itemText">{task.title}</span>
-            <div className="itemDeleteIcon" onClick={() => handleDelete(task.id)}>X</div>
+            <span className={`itemText ${task.completed ? 'itemComplete' : ''}`}>{task.title}</span>
+            <div className='itemButtonsContainer'>
+              {task.completed ? (
+                <CiUndo className="itemUndoIcon" size={24} onClick={() => handleToggle(task.id)} />
+              ) : (
+                <BsCheckLg className="itemToggleIcon" size={24} onClick={() => handleToggle(task.id)} />
+              )}
+              <RxCross2 className="itemDeleteIcon" size={24} onClick={() => handleDelete(task.id)} />
+            </div>
           </div>
         ))}
       </div>
