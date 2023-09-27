65232acb7585a29bd19ad456f7f73383fce4e999
Dave Amison <dave.amison@sixstar.global>
2023-09-27 13:29:54 +0000

    tasks remember there original position on toggle

--- src/redux/slices/tasksSlice.js
+++ src/redux/slices/tasksSlice.js
@@ -8,16 +8,19 @@ const tasksSlice = createSlice({
             id: uuidv4(),
             title: 'Buy milk',
             completed: false,
+            order: 1,
         },
         {
             id: uuidv4(),
             title: 'Buy eggs',
             completed: false,
+            order: 2,
         },
         {
             id: uuidv4(),
             title: 'Buy bread',
             completed: false,
+            order: 3,
         },
     ],
     reducers: {
@@ -26,8 +29,19 @@ const tasksSlice = createSlice({
                 id: uuidv4(),
                 title: action.payload.title,
                 completed: false,
+                order: state.length + 1,
             };
-            state.push(newTask);
+            const completedTasks = state.filter((task) => task.completed);
+            completedTasks.push(newTask);
+            const incompleteTasks = state.filter((task) => !task.completed);
+            state.length = 0;
+
+            incompleteTasks.sort((a, b) => a.order - b.order);
+            completedTasks.sort((a, b) => a.order - b.order);
+
+            incompleteTasks.forEach((task) => state.push(task));
+            completedTasks.forEach((task) => state.push(task));
+
         },
         toggleTask: (state, action) => {
             const task = state.find((task) => task.id === action.payload.id);
@@ -37,14 +51,28 @@ const tasksSlice = createSlice({
             const completedTasks = state.filter((task) => task.completed);
             const incompleteTasks = state.filter((task) => !task.completed);
             state.length = 0;
+
+            incompleteTasks.sort((a, b) => a.order - b.order);
+            completedTasks.sort((a, b) => a.order - b.order);
+
             incompleteTasks.forEach((task) => state.push(task));
             completedTasks.forEach((task) => state.push(task));
+
         },
         deleteTask: (state, action) => {
             const index = state.findIndex((task) => task.id === action.payload.id);
             if (index !== -1) {
                 state.splice(index, 1);
             }
+            const completedTasks = state.filter((task) => task.completed);
+            const incompleteTasks = state.filter((task) => !task.completed);
+            state.length = 0;
+
+            incompleteTasks.sort((a, b) => a.order - b.order);
+            completedTasks.sort((a, b) => a.order - b.order);
+
+            incompleteTasks.forEach((task) => state.push(task));
+            completedTasks.forEach((task) => state.push(task));
         },
     },
 });
