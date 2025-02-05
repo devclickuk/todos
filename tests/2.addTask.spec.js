1677e18e820fc8738b5ee17fff7e9d9ab4222781
Dave Amison <dave_amison@icloud.com>
2025-02-05 18:03:26 +0000

    Refactor Playwright test scripts and update package.json

---
+++ tests/2.addTask.spec.js
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Add Task', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:3000/todos');
+    });
+
+    // check that there are 3 list items
+    test('check that there are 3 list items', async ({ page }) => {
+        const listItems = page.locator('[data-testid^="listItem-"]');
+        await expect(listItems).toHaveCount(3);
+    });
+
+    test('add Buy Ham to the list', async ({ page }) => {
+        // add the text 'Buy Ham' to the input
+        const addInput = page.getByTestId('addInput');
+        await addInput.fill('Buy Ham');
+
+        // click the add button
+        const addButton = page.getByTestId('addButton');
+        await addButton.click();
+
+        // check that the list now has 4 items
+        const listItems = page.locator('[data-testid^="listItem-"]');
+        await expect(listItems).toHaveCount(4);
+
+        // check that the new item is visible
+        const listItem = page.getByTestId('listItem-4');
+        await expect(listItem).toBeVisible();
+
+        // check that the new item has the text 'Buy Ham'
+        await expect(listItem).toHaveText('Buy Ham');
+
+        // check that the input is empty after adding the item
+        await expect(addInput).toHaveAttribute('placeholder', 'Add a task');
+    });
+
+});
---
+++ tests/3.deleteTask.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Delete Task', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:3000/todos');
+    });
+
+    // check that there are 3 list items
+    test('check that there are 3 list items', async ({ page }) => {
+        const listItems = page.locator('[data-testid^="listItem-"]');
+        await expect(listItems).toHaveCount(3);
+    });
+
+    test('delete the first item', async ({ page }) => {
+        // click the delete icon of the first item
+        const itemDeleteIcon = page.getByTestId('itemDeleteIcon-2');
+        await itemDeleteIcon.click();
+
+        // check that the list now has 2 items
+        const listItems = page.locator('[data-testid^="listItem-"]');
+        await expect(listItems).toHaveCount(2);
+
+        // check that the two remaining items have text 'Buy milk' and 'Buy bread'
+        const listItem1 = page.getByTestId('listItem-1');
+        await expect(listItem1).toHaveText('Buy milk');
+
+        const listItem2 = page.getByTestId('listItem-2');
+        await expect(listItem2).toHaveText('Buy bread');
+    });
+});
